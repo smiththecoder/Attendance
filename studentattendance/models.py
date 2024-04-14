80e297789974f7f2d3927e4599318befa6ef877b
smiththecoder <166221106+smiththecoder@users.noreply.github.com>
2024-04-13 23:23:34 -0400

    Update models.py

--- studentattendance/models.py
+++ studentattendance/models.py
@@ -7,3 +7,17 @@ class Rollno(models.Model):
     
     def __str__(self):
         return self.roll
+
+class Final(models.Model):
+    roll = models.CharField(max_length=10)
+    date = models.DateTimeField(default=datetime.now(), blank=True) 
+
+    def __str__(self):
+        return self.roll
+
+class Apii(models.Model):
+    roll = models.CharField(max_length=10)
+    date = models.DateTimeField(default=datetime.now(), blank=True) 
+
+    def __str__(self):
+        return self.roll
