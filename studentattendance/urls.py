49b921b68d0f8bec82082914076b369c0ffb0f6a
smiththecoder <166221106+smiththecoder@users.noreply.github.com>
2024-04-13 23:16:51 -0400

    Update urls.py

--- studentattendance/urls.py
+++ studentattendance/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home,main,res
+from .views import home,main,res,apid
 
 urlpatterns = [
     path('',home),
     path('main',main),
-    path('result',res)
+    path('result',res),
+    path('api/<str:roll>',apid)
 ]
