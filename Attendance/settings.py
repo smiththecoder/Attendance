b84e0f9d11a711e91e05f9cae5811be285712338
smiththecoder <166221106+smiththecoder@users.noreply.github.com>
2024-04-14 01:25:20 -0400

    Update settings.py

--- Attendance/settings.py
+++ Attendance/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'rest_framework',
     'studentattendance.apps.StudentattendanceConfig',
     'django.contrib.admin',
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
